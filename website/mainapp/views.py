b7daa986113ead5697417bb2db3fdf7d2ba2705a
Eduardo Escárez <eduardoe@gmail.com>
2023-06-15 01:52:54 -0400

    Formulario de creación funcionando

--- website/mainapp/views.py
+++ website/mainapp/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
@@ -16,3 +17,19 @@ class UsersView(TemplateView):
     def get(self, request, *args, **kwargs):
         users = User.objects.all()
         return render(request, self.template_name, {'users': users, 'titulo' : 'Usuarios'})
+    
+class CreateUsersView(TemplateView):
+    template_name = 'createusers.html'
+
+    def get(self, request, *args, **kwargs):
+        formulario = UserCreationForm(request.POST)
+        return render(request, self.template_name, {'formulario': formulario, 'titulo': 'Formulario de creación de usuarios',})
+
+    def post(self, request, *args, **kwargs):
+        formulario = UserCreationForm(request.POST)
+        if formulario.is_valid():
+            formulario.save()
+            mensajes = {'enviado': True, 'resultado': 'El usuario se ha creado correctamente'}
+        else:
+            mensajes = {'enviado': False, 'resultado': formulario.errors}
+        return render(request, self.template_name, {'formulario': formulario, 'mensajes': mensajes, 'titulo': 'Formulario de creación de usuarios',})
